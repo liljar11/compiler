ec184e9b37b1b0604abfdd03eecd7d92f5f0a086
BerglindLilja <berglindliljab@gmail.com>
2016-02-11 16:42:01 +0000

    hello

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,9 +1,5 @@
 package compiler;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-
 public class Compiler {
 
 	public static void main(String[] args) {
@@ -13,20 +9,4 @@ public class Compiler {
 		myParser.parse();
 		
 	}
-	File inputfile = new File("input.txt");
-	File outputfile = new File("output.l");
-	
-	FileReader fr = new FileReader(inputfile); //les file-inn inn i array
-	char[] arr = new char[50];
-	fr.read(arr);
-	
-	
-	
-	
-	
-	
-	FileWriter writer = new FileWriter(outputfile); //les shittið inn i fileinn sem er sendur i interpreter
-	writer.write("eitthvaðsniðugt");
-	writer.flush();
-	writer.close();
 }
---
+++ src/compiler/Interpreter.java
@@ -0,0 +1,40 @@
+package compiler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Interpreter {
+	
+	private Map<String, Integer> map = new HashMap<String, Integer>(); //we use a map class to store values of identifiers
+	private Stack<String> stack = new Stack<String>(); //we use the stack class to process the intermediate code
+	
+	
+	public static void main(String[] args) {
+	
+		Interpreter in = new Interpreter();
+	}
+	
+	/*public String fetch(){
+		//taka við þvi sem compiler skilar eina línu í einu
+		return 
+	}
+	
+	
+	public String decode(fetch){
+		
+		
+		return String[]
+	}
+	
+	public String execute(String[]){
+		
+		//execute þú ber saman input við það sem er intermediate code, tjékkar hvort fyrsti stakur í arraeyinu se 
+		//PUSH eða eitthavð annað
+	}*/
+	
+	
+	
+//interpeter tekur vi� output fr� compiler � formatinu sem er � bls 3 � pdf
+//og notar token a�ger�irnar og skilar �tkomunni reikna�ri.
+}
